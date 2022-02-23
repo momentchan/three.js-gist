c156bb1c62d98a30d10cab4acf1c65926af34c1b
hungmingjyun <hungmingjyun@team-lab.com>
2022-02-24 08:52:48 +0900

    update camera base

--- Common/CameraBase.js
+++ Common/CameraBase.js
@@ -14,12 +14,11 @@ export default class CameraBase {
 
     setInstance() {
         this.instance = new THREE.PerspectiveCamera(
-            35,
-            this.sizes.width / this.sizes.height,
-            0.1,
-            100
-        )
-        this.instance.position.set(6, 4, 8)
+            75, 
+            this.sizes.width / this.sizes.height, 
+            0.1, 
+            100)
+        this.instance.position.z = 3
         this.scene.add(this.instance)
     }
 
